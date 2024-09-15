99f00a3457063a300a68ce04550f865bdb2cf701
Udayabhanu2514 <udayabhanureddy2004@gmail.com>
2024-09-15 11:45:30 +0530

    Your commit message here

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -124,7 +124,7 @@ const Footer = () => {
           </Col>
 
           <Col lg='12' className='text-center pt-5'>
-            <p className='copyright'>Copyright {year}, design and develop by JayaVardhan.All rights reserved.</p>
+            <p className='copyright'>Copyright {year}, design and develop by Udhaybhanu .All rights reserved.</p>
           </Col>
         </Row>
       </Container>
